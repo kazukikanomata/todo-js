33d881219b3f0e95dd580346d373ec3ccb02b8c0
kazukikanomata <77597098+kazukikanomata@users.noreply.github.com>
2022-08-24 22:56:52 +0900

    task delete funciton

--- src/index.js
+++ src/index.js
@@ -23,7 +23,9 @@ const onClickAdd = () => {
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除");
+    // 押された削除ボタンの親タグを未完了リストから削除
+    const delteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(delteTarget);
   });
 
   // div　タグの子要素に各要素を設定する
